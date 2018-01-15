d2aefcb6a8013e93f4d8c2b45113a18a3baab4dd
Paul Brown <paabrown@gmail.com>
2018-01-15 16:33:52 -0500

    Allow node to loop over files in src and dest folders for compilation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
+const path = require('path');
 const csv = require('csv');
-
-const { source, destination } = require('config.js');
+const { source, destination } = require('./config.js');
 
 const parseOptions = {
   columns: true
@@ -10,52 +10,101 @@ const parseOptions = {
 fs.readdir(source, (err, srcFiles) => {
   console.log('err0', err);
   console.log('srcFiles', srcFiles);
-  
-  fs.readdir(destination, (err))
+
+  fs.readdir(destination, (err, destFiles) => {
+    console.log('err1', err);
+    console.log('destFiles', destFiles);
+
+    srcFiles.forEach((srcFileName, i) => {
+      if (/\.csv$/.test(srcFileName)) {
+        fs.readFile(path.join(source, srcFileName), 'utf8', (err, dataSrc) => {
+          console.log('err2', err);
+          console.log('dataSrc', dataSrc);
+
+          if (destFiles.includes(srcFileName)) {
+            fs.readFile(path.join(dest, srcFileName), 'utf8', (err, dataDest) => {
+              console.log('err3', err);
+
+              csv.parse(dataSrc, parseOptions, (err, dataSrc) => {
+                console.log('err4', err)
+              
+                csv.parse(dataDest, parseOptions, (err, dataDest) => {
+                  console.log('dataDest', dataDest);
+                  const noDups = {};
+          
+                  dataSrc.forEach(row => {
+                    let key = row['Session ID'] + row['Transfer ID']
+                    key.length ? noDups[key] = row : null;
+                  });
+          
+                  dataDest.forEach(row => {
+                    let key = row['Session ID'] + row['Transfer ID']
+                    key.length ? noDups[key] = row : null;
+                  });
+          
+                  const finalData = Object.keys(noDups).map(key => noDups[key]);
+                  
+                  csv.stringify(finalData, (err, output) => {
+                    console.log('err5', err)
+                    console.log('output', output);
+                    let finalOutput = 'TimeStamp,HotFolder,Transfer Type,Direction,Local ID,Location,Remote ID,Remote Location,Session ID,Transfer ID,Status,File Size,Data Transferred,Transfer Start time,Transfer stop time,Average Rate (kbps),Peak Rate (kbps),Effective Rate (kbps),Packet Loss %,Remote Host,File Path,Username,Task ID,Task Name,Report Time' + output;
+                    fs.writeFile(path.join(dest, srcFileName), finalOutput, (err) => console.log('err6', err)) 
+                  });
+                });
+              });
+            });
+
+          } else {
+            fs.writeFile(path.join(dest, srcFileName), dataSrc, (err) => console.log('err6', err));
+          }
+        })
+      }
+    })
+  })
 })
 
-fs.readFile('./source/realDataSrc.csv', 'utf8', (err, dataSrc) => {
-  console.log('err1', err);
+// fs.readFile('./source/realDataSrc.csv', 'utf8', (err, dataSrc) => {
+//   console.log('err1', err);
 
-  fs.readFile('./dest/realDataDest.csv', 'utf8', (err, dataDest) => {
-    console.log('err2', err);
-    console.log('dataDest1', dataDest);
+//   fs.readFile('./dest/realDataDest.csv', 'utf8', (err, dataDest) => {
+//     console.log('err2', err);
+//     console.log('dataDest1', dataDest);
   
-    csv.parse(dataSrc, parseOptions, (err, dataSrc) => {
-      console.log('err3', err)
+//     csv.parse(dataSrc, parseOptions, (err, dataSrc) => {
+//       console.log('err3', err)
 
-      csv.parse(dataDest, parseOptions, (err, dataDest) => {
-        console.log('dataDest', dataDest);
-        const noDups = {};
+//       csv.parse(dataDest, parseOptions, (err, dataDest) => {
+//         console.log('dataDest', dataDest);
+//         const noDups = {};
 
-        dataSrc.forEach(row => {
-          let key = row['Session ID'] + row['Transfer ID']
-          key.length ? noDups[key] = row : null;
-        })
+//         dataSrc.forEach(row => {
+//           let key = row['Session ID'] + row['Transfer ID']
+//           key.length ? noDups[key] = row : null;
+//         })
 
-        dataDest.forEach(row => {
-          let key = row['Session ID'] + row['Transfer ID']
-          key.length ? noDups[key] = row : null;
-        })
+//         dataDest.forEach(row => {
+//           let key = row['Session ID'] + row['Transfer ID']
+//           key.length ? noDups[key] = row : null;
+//         })
 
-        const finalData = Object.keys(noDups).map(key => noDups[key]);
+//         const finalData = Object.keys(noDups).map(key => noDups[key]);
         
-        csv.stringify(finalData, (err, output) => {
-          console.log('err4', err)
-          console.log('output', output);
-          let finalOutput = 'TimeStamp,HotFolder,Transfer Type,Direction,Local ID,Location,Remote ID,Remote Location,Session ID,Transfer ID,Status,File Size,Data Transferred,Transfer Start time,Transfer stop time,Average Rate (kbps),Peak Rate (kbps),Effective Rate (kbps),Packet Loss %,Remote Host,File Path,Username,Task ID,Task Name,Report Time' + output;
-          fs.writeFile('./dest/realDataDest.csv', finalOutput, (err) => console.log('err5', err)) 
-        });
-      });
-    });
-  });
-});
-  // console.log('dataSrc', dataSrc);
+//         csv.stringify(finalData, (err, output) => {
+//           console.log('err4', err)
+//           console.log('output', output);
+//           let finalOutput = 'TimeStamp,HotFolder,Transfer Type,Direction,Local ID,Location,Remote ID,Remote Location,Session ID,Transfer ID,Status,File Size,Data Transferred,Transfer Start time,Transfer stop time,Average Rate (kbps),Peak Rate (kbps),Effective Rate (kbps),Packet Loss %,Remote Host,File Path,Username,Task ID,Task Name,Report Time' + output;
+//           fs.writeFile('./dest/realDataDest.csv', finalOutput, (err) => console.log('err5', err)) 
+//         });
+//       });
+//     });
+//   });
+// });
+//   // console.log('dataSrc', dataSrc);
 
 
 
-  // fs.appendFile('./dest/dest1.csv', data, (err) => {
-  //   console.log('err2', err)
-  //   console.log('all done!');
-  // })
+//   // fs.appendFile('./dest/dest1.csv', data, (err) => {
+//   //   console.log('err2', err)
+//   //   console.log('all done!');
+//   // })
 
