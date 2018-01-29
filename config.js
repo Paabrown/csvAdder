75e0b017aa2a934eaee66a97914cafe8c8abb0e6
Paul Brown <paabrown@gmail.com>
2018-01-28 22:54:39 -0500

    fix for nested folders

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   source: './source',
   destination: './dest',
+  srcFileName: 'hotfolder_data_report.csv',
   headers: ['TimeStamp','HotFolder','Transfer Type','Direction','Local ID','Location','Remote ID','Remote Location','Session ID','Transfer ID','Status','File Size','Data Transferred','Transfer Start time','Transfer stop time','Average Rate (kbps)','Peak Rate (kbps)','Effective Rate (kbps)','Packet Loss %','Remote Host','File Path','Username','Task ID','Task Name','Report Time']
 }
--- csvAdd.js
+++ csvAdd.js
@@ -1,25 +1,31 @@
 const fs = require('fs');
 const path = require('path');
 const csv = require('csv');
-const { source, destination, headers } = require('./config.js');
+const { source, destination, headers, srcFileName } = require('./config.js');
 
 const csvAdd = function() {
   const parseOptions = {
     columns: true
   }
-  // get an array of files from source directory
-  fs.readdir(source, (err, srcFiles) => {
+  // get an array of folders from source directory
+  fs.readdir(source, (err, srcFolders) => {
 
-  // get an array 
+    // get an array of files in the destination directory
     fs.readdir(destination, (err, destFiles) => {
 
-      srcFiles.forEach((srcFileName, i) => {
-        if (/\.csv$/.test(srcFileName)) {
-          fs.readFile(path.join(source, srcFileName), 'utf8', (err, dataSrc) => {
-            
-            if (destFiles.includes(srcFileName)) {
+      srcFolders.forEach((srcFolderName, i) => {
+        const srcFilePath = path.join(source, srcFolderName, srcFileName);
+        const destFileName = srcFolderName + '.csv';
+        const destFilePath = path.join(destination, destFileName);
 
-              fs.readFile(path.join(destination, srcFileName), 'utf8', (err, dataDest) => {
+        let fileExists = fs.existsSync(srcFilePath);
+
+        if (/\.csv$/.test(srcFilePath) && fileExists) {
+          fs.readFile(srcFilePath, 'utf8', (err, dataSrc) => {
+            
+            if (destFiles.includes(destFileName)) {
+
+              fs.readFile(destFilePath, 'utf8', (err, dataDest) => {
 
                 csv.parse(dataSrc, parseOptions, (err, dataSrc) => {
                 
@@ -40,14 +46,14 @@ const csvAdd = function() {
                     
                     csv.stringify(finalData, (err, output) => {
                       let finalOutput = headers.join(',') + '\n' + output;
-                      fs.writeFile(path.join(destination, srcFileName), finalOutput, (err) => console.log('err6', err)); 
+                      fs.writeFile(destFilePath, finalOutput, (err) => console.log('err6', err)); 
                     });
                   });
                 });
               });
 
             } else {
-              fs.writeFile(path.join(destination, srcFileName), dataSrc, (err) => console.log('err6', err));
+              fs.writeFile(destFilePath, dataSrc, (err) => console.log('err6', err));
             }
           })
         }
