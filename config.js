0d8a137be4d692a653850ea0ff0c557b5ecb08f6
Paul Brown <paabrown@gmail.com>
2018-01-28 22:59:39 -0500

    test

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  source: './source',
+  source: './sourc',
   destination: './dest',
   srcFileName: 'hotfolder_data_report.csv',
   headers: ['TimeStamp','HotFolder','Transfer Type','Direction','Local ID','Location','Remote ID','Remote Location','Session ID','Transfer ID','Status','File Size','Data Transferred','Transfer Start time','Transfer stop time','Average Rate (kbps)','Peak Rate (kbps)','Effective Rate (kbps)','Packet Loss %','Remote Host','File Path','Username','Task ID','Task Name','Report Time']
